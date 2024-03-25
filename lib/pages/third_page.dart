547be730e1b9989d17f67d1336cf6492e6ce3355
Cornel Lupu <cornellupu@hotmail.com>
2024-03-24 18:39:06 -0700

    grouped list

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -13,10 +13,18 @@ class AllGroups {
 
     return count;
   }
+
+  int get allTtemsCount {
+    return itemsCount + groups.length;
+  }
 }
 
 class ItemsGroup {
+  String name = '';
   List<Item> items = [];
+  Color color;
+
+  ItemsGroup({required this.name, required this.color});
 }
 
 class Item {
@@ -29,11 +37,13 @@ class Item {
 }
 
 class ThirdPage extends StatelessWidget {
-  AllGroups allGroups;
+  late AllGroups allGroups;
 
+  // initialize with content
   ThirdPage({super.key}) {
     allGroups = AllGroups();
-    ItemsGroup notCompleted = ItemsGroup();
+    ItemsGroup notCompleted =
+        ItemsGroup(name: 'Not completed', color: Colors.greenAccent);
 
     allGroups.groups.add(notCompleted);
 
@@ -53,7 +63,8 @@ class ThirdPage extends StatelessWidget {
     notCompleted.items.add(Item(
         name: 'Once7', subtitle: 'updated 22 minutes ago', completed: false));
 
-    ItemsGroup completed = ItemsGroup();
+    ItemsGroup completed =
+        ItemsGroup(name: 'Completed', color: Colors.orangeAccent);
 
     allGroups.groups.add(completed);
 
@@ -92,30 +103,92 @@ class ThirdPage extends StatelessWidget {
         backgroundColor: Colors.grey[100],
       ),
       body: ListView.builder(
-        itemCount: allGroups.itemsCount,
+        itemCount: allGroups.allTtemsCount,
         itemBuilder: (BuildContext context, int index) {
-          Item item = items[index];
+          ItemsGroup group;
+          Item item;
 
-          if (index == 0) {
+          // find the group and the item for this index
+          int items = 0;
+          int gr = 0;
+
+          while (items < index) {
+            group = allGroups.groups[gr];
+            if (group.items.length + items < index) {
+              items += group.items.length + 1;
+              gr++;
+            } else {
+              break;
+            }
+          }
+          if (items == index) {
+            // render the header for group gr
             return Container(
               color: Colors.white54,
-              margin: const EdgeInsets.all(8),
-              child: const Text('My Turn'),
+              margin: const EdgeInsets.all(6),
+              child: Text(
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  '${allGroups.groups[gr].name} (${allGroups.groups[gr].items.length})'),
             );
+          } else {
+            // render item index - items - 1
+            item = allGroups.groups[gr].items[index - items - 1];
+            return Blade(allGroups: allGroups, gr: gr, item: item);
           }
-
-          return Container(
-            color: Colors.greenAccent,
-            margin: const EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(item.name),
-                Text(item.subtitle),
-              ],
-            ),
-          );
         },
+      ),
+    );
+  }
+}
+
+class Blade extends StatelessWidget {
+  const Blade({
+    super.key,
+    required this.allGroups,
+    required this.gr,
+    required this.item,
+  });
+
+  final AllGroups allGroups;
+  final int gr;
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('tapped on item ${item.name}');
+      }, // Handle your callback
+
+      child: Container(
+        decoration: BoxDecoration(
+            color: allGroups.groups[gr].color,
+            border: Border.all(
+              color: allGroups.groups[gr].color,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.4),
+                spreadRadius: 4,
+                blurRadius: 7,
+                offset: const Offset(3, 3), // changes position of shadow
+              ),
+            ],
+            borderRadius: const BorderRadius.all(Radius.circular(3))),
+        margin: const EdgeInsets.fromLTRB(6, 2, 10, 10),
+        child: Padding(
+          padding: const EdgeInsets.all(3),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(item.name),
+              Text(item.subtitle),
+            ],
+          ),
+        ),
       ),
     );
   }
